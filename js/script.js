3da13ff57c4f3ffc5b20cd9afb71eb8d4c9d8cb7
wisnawa <putu.wisnawa@gmail.com>
2023-07-17 17:22:52 +0800

    modified files

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-// Toggle  class active
+// Toggle  class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di click
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+};
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
 };
 // click di luar side bar hilangkan navbar
 const hamburger = document.getElementById("hamburger-menu");
